ce1ba631fbe44e26bd9cd3b1ae5062df6f67103f
Rizz9y <2310501053@mahasiswa.upnvj.ac.id>
2025-06-20 08:39:29 +0700

    Delete config directory

--- config/jwt.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  secret:
-    process.env.JWT_SECRET || "fallback_secret_for_development_only_change_me",
-  expiresIn: process.env.JWT_EXPIRES_IN || "1h", // Token expiration time
-};
